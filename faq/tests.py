1c6771622b395f3e87f8ad222bc73c29ff6f1f8d
Nikhil Singh <nikhil.2226csit1110@kiet.edu>
2025-02-02 00:08:33 +0530

    feat: Add test and recheck

--- faq/tests.py
+++ faq/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+import pytest
+from django.test import Client
+from .models import FAQ
 
-# Create your tests here.
+@pytest.mark.django_db
+def test_create_faq():
+    faq = FAQ.objects.create(question="Test?", answer="This is a test.")
+    assert faq.question == "Test?"
+
+@pytest.mark.django_db
+def test_api_response():
+    client = Client()
+    response = client.get('/api/faqs/?lang=hi')
+    assert response.status_code == 200
